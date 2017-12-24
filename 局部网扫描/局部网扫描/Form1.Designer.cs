f34c3a1e0c8335f86495b9d1d21922b139a609db
wl4837 <wl4837@163.com>
2017-12-24 14:50:56 +0800

    Add files via upload

---
+++ 局部网扫描/局部网扫描/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace 局部网扫描
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(420, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "检索主机";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.80747F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.19254F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(793, 509);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(787, 97);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(160, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 31);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(160, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 31);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(47, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "终点IP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(47, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "起始IP";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 106);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(787, 364);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(787, 364);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "IP地址";
+            this.columnHeader5.Width = 242;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "主机";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 266;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.progressBar1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 476);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(787, 30);
+            this.panel3.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(787, 30);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.VisibleChanged += new System.EventHandler(this.progressBar1_VisibleChanged);
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 509);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "局域网主机扫描";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ 局部网扫描/局部网扫描/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace 局部网扫描
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Truncate_IP(string IP,bool type_bool) {
+            string Truncate = "";
+            if (type_bool == true)
+            {
+                MatchCollection matchCollection = Regex.Matches(IP, "[0-9]{1,3}");
+                foreach (Match item in matchCollection)
+                {
+                    Truncate = item.Value;
+                }
+            }
+            else
+            {
+                MatchCollection matchCollection = Regex.Matches(IP, "[0-9]{1,3}");
+                int Item_count = 0;
+                foreach (Match item in matchCollection)
+                {
+                    ++Item_count;
+                    if (matchCollection.Count == Item_count)
+                    {
+
+                    }
+                    else
+                    {
+
+                        Truncate += (item.Value + ".");
+                    }
+                }
+        }
+
+            return Truncate;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Thread thread = new Thread(new ThreadStart(button1Thread));
+            thread.Start();
+        }
+
+        private void button1Thread() {
+            listView1.Items.Clear();
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            progressBar1.Value = 0;
+            if (IPAddress.TryParse(textBox1.Text, out IPAddress iP) && IPAddress.TryParse(textBox2.Text, out IPAddress ip))
+            {
+                progressBar1.Maximum = 1 + (int.Parse(Truncate_IP(textBox2.Text, true)) - int.Parse(Truncate_IP(textBox1.Text, true)));
+                for (int i = int.Parse(Truncate_IP(textBox1.Text, true)); i <= int.Parse(Truncate_IP(textBox2.Text, true)); i++)
+                {
+                    Thread.Sleep(20);
+                    ++progressBar1.Value;
+                    Control.CheckForIllegalCrossThreadCalls = false;
+                    Thread thread = new Thread(new ParameterizedThreadStart(retrieval));
+                    thread.Start(Truncate_IP(textBox1.Text, false) + i);
+                    if (progressBar1.Value == progressBar1.Maximum)
+                    {
+                        button1.Enabled = true;
+                        textBox1.Enabled = true;
+                        textBox2.Enabled = true;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("IP地址不合法");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void retrieval(object pathinfo) {
+            try
+            {
+                ListViewItem viewItem = new ListViewItem(pathinfo.ToString());
+                viewItem.SubItems.Add(Dns.GetHostByAddress(pathinfo.ToString()).HostName);
+                listView1.Items.Add(viewItem);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //获取起始地址s
+           MatchCollection matchCollection = Regex.Matches(Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString(), "[0-9]{1,3}");
+            int Item_count = 0;
+            foreach (Match item in matchCollection)
+            {
+                ++Item_count;
+                if (matchCollection.Count == Item_count)
+                {
+                    textBox1.AppendText("1");
+                }
+                else
+                {
+                    switch (item.Length)
+                    {
+                        case 1:
+                            textBox1.AppendText(item.Value + ".");
+                            break;
+                        case 2:
+                            textBox1.AppendText(item.Value + ".");
+                            break;
+                        default:
+                            textBox1.AppendText(item.Value + ".");
+                            break;
+                    }
+                    
+                }
+            }
+        }
+
+        private void Ip_2(string Ip_1) {
+            textBox2.Text = null;
+            MatchCollection matchCollection = Regex.Matches(Ip_1, "[0-9]{1,3}");
+            int Item_count = 0;
+            foreach (Match item in matchCollection)
+            {
+                ++Item_count;
+                if (matchCollection.Count == Item_count)
+                {
+                    textBox2.AppendText("255");
+                }
+                else
+                {
+                    textBox2.AppendText(item.Value + ".");
+                }
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            Ip_2(textBox1.Text);
+        }
+
+        private void progressBar1_VisibleChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
